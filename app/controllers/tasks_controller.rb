d1110a85fa4509f6e155ce6e5dcded396823784a
naoki.hannzawa <taro.kirameki@techacademy.jp>
2020-11-03 07:33:19 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,22 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
  def index#レコードの一覧表示
-   @task = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
  end
 
  def show#レコード一つ表示
+   @task = current_user.tasks.find_by(id: params[:id])
  end
 
  def new#新規作成ページ
-   @task = Task.new
+   @task = current_user.tasks.build
  end
 
  def create#newを処理する
-  @task = Task.new(task_params)
+  @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました'
       redirect_to @task
@@ -23,10 +27,11 @@ class TasksController < ApplicationController
  end
 
  def edit
+  @task = current_user.tasks.find_by(id: params[:id])
  end
 
  def update
-
+  @task = current_user.tasks.find_by(id: params[:id])
     if @task.update(task_params)
       flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
@@ -37,8 +42,8 @@ class TasksController < ApplicationController
  end
 
  def destroy
+   @task = current_user.tasks.find_by(id: params[:id])
    @task.destroy
-
    flash[:success] = 'タスクは正常に削除されました'
    redirect_to tasks_url
  end
@@ -49,8 +54,11 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
  end
  
- def set_task
-   @task = Task.find(params[:id])
+ def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
+  
   resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  resources :users, only: [:new, :create]
 end
