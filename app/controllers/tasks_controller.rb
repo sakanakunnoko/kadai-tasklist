88203de1a61d8e9cba9b677817f648b88a1d2f64
naoki.hannzawa <taro.kirameki@techacademy.jp>
2020-11-07 12:14:02 +0000

    small changes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user #,only: [:destroy]
+  before_action :correct_user ,only: [:destroy]
  #レコードの一覧表示
  def index
    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
